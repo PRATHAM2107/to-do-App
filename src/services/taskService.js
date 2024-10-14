956d56c6617ca314d97389605d4836630d710e04
PRATHAM2107 <prathameshpatil0303@gmail.com>
2024-10-14 21:43:52 +0530

    Final

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -4,7 +4,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 // Get all tasks
 export const getTasks = async () => {
   try {
-    const response = await axios.get(API_URL);
+    const response = await axios.get(`${API_URL}/tasks`);
     return response.data;
   } catch (error) {
     throw error;
@@ -14,7 +14,7 @@ export const getTasks = async () => {
 // Create a new task
 export const createTask = async (task) => {
   try {
-    const response = await axios.post(API_URL, task);
+    const response = await axios.post(`${API_URL}/tasks`, task);
     return response.data;
   } catch (error) {
     throw error;
@@ -24,7 +24,7 @@ export const createTask = async (task) => {
 // Update a task
 export const updateTask = async (id, updatedTask) => {
   try {
-    const response = await axios.put(`${API_URL}/${id}`, updatedTask);
+    const response = await axios.put(`${API_URL}/tasks/${id}`, updatedTask);
     return response.data;
   } catch (error) {
     throw error;
@@ -34,7 +34,7 @@ export const updateTask = async (id, updatedTask) => {
 // Delete a task
 export const deleteTask = async (id) => {
   try {
-    await axios.delete(`${API_URL}/${id}`);
+    await axios.delete(`${API_URL}/tasks/${id}`);
   } catch (error) {
     throw error;
   }
